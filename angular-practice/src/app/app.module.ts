d568aaa94f3146ffc88c283ea0a361f2d6c49345
rahat854 <rahatkabir20@gmail.com>
2021-07-17 23:52:07 +0600

    added to app module

--- angular-practice/src/app/app.module.ts
+++ angular-practice/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {serverComponent} from "./server/server.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    serverComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
