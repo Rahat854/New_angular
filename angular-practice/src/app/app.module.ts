a2ff8dae0a50908fb136f4e208739a569b74c6e0
rahat854 <rahatkabir20@gmail.com>
2021-07-19 15:41:49 +0600

    added two components

--- angular-practice/src/app/app.module.ts
+++ angular-practice/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {serverComponent} from "./server/server.component";
 import { ServersComponent } from './servers/servers.component';
+import { WaringAlertComponent } from './waring-alert/waring-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     serverComponent,
-    ServersComponent
+    ServersComponent,
+    WaringAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-practice/src/app/waring-alert/waring-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-waring-alert',
+  templateUrl: './waring-alert.component.html',
+  styleUrls: ['./waring-alert.component.scss']
+})
+export class WaringAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
