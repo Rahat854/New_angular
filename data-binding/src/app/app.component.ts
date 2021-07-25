87a6500bdc2f44be340035ea83fec9ca9e991428
rahat854 <rahatkabir20@gmail.com>
2021-07-26 00:23:07 +0600

    binding to custom event

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -8,4 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'data-binding';
   serverElements = [{type: 'server', name:'Testserver', content: 'just a test'}]
+  onServerAdded(serverData:{serverName: string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    })
+  }
+  onBlueprintAdded(serverData:{serverName:string, serverContent:string}) {
+    this.serverElements.push({
+      type:'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    })
+  }
 }
